35accf4a52fc037ebf0bae56e97a24202dbc6655
kaido <maxartem0419@gmail.com>
2021-12-23 21:53:36 +0300

    feat(Modal): create modal

--- src/components/ModalWindow/index.vue
+++ src/components/ModalWindow/index.vue
@@ -2,18 +2,21 @@
   <button @click="openModalHandler">Добавить пользователя</button>
 
   <teleport to="body">
-    <div v-if="isOpenModal" class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
+    <div v-if="isOpenModal" class="modal__mask">
+      <div class="modal__wrapper">
+        <div class="modal__container">
+          <button @click="closeModalHandler" class="modal__close">
+            <CloseIcon />
+          </button>
+          <div class="modal__header">
             <h2>Добавить пользователя</h2>
           </div>
 
-          <div class="modal-body">
+          <div class="modal__body">
             <p>Основной контент</p>
           </div>
 
-          <div class="modal-footer">
+          <div class="modal__footer">
             <p>Подвал</p>
             <button class="modal-default-button" @click="closeModalHandler">
               OK
@@ -27,7 +30,13 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+
+import CloseIcon from "@/assets/icons/Close.vue";
+
 export default defineComponent({
+  components: {
+    CloseIcon,
+  },
   setup: () => {
     console.log("Ghbdtn");
 
@@ -51,53 +60,41 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-}
+.modal {
+  &__mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+  }
 
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
+  &__wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
 
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  font-family: Helvetica, Arial, sans-serif;
-}
+  &__container {
+    position: relative;
+    max-width: 800px;
+    width: 100%;
+    margin: 0px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    font-family: Helvetica, Arial, sans-serif;
+  }
 
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  display: block;
-  margin-top: 1rem;
-}
-
-.modal-enter-active,
-.modal-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.modal-enter-from,
-.modal-leave-to {
-  opacity: 0;
+  &__close {
+    position: absolute;
+    top: 0;
+    right: -30px;
+    border: none;
+    background: rgba(0, 0, 0, 0);
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "@/styles/css/normalize.css";
+import "@/styles/scss/globals.scss";
+
 createApp(App).mount("#app");
